c9d4552b703804ae3b73974b0a0266c416f2b349
sven <bohms@ideomatic.io>
2021-05-07 18:18:14 -0400

    parse using Float.parse to recover from errors

--- lib/airflow/parser.ex
+++ lib/airflow/parser.ex
@@ -13,11 +13,22 @@ defmodule Airflow.Parser do
 
   def parse(string) do
     [_address, pressure, temperature, volumetric_flow, mass_flow, setpoint, gas] = String.split(string)
-    %Airflow{pressure: String.to_float(pressure),
-      temperature: String.to_float(temperature),
-      volumetric_flow: String.to_float(volumetric_flow),
-      mass_flow: String.to_float(mass_flow),
-      setpoint: String.to_float(setpoint),
+    %Airflow{pressure: to_float(pressure),
+      temperature: to_float(temperature),
+      volumetric_flow: to_float(volumetric_flow),
+      mass_flow: to_float(mass_flow),
+      setpoint: to_float(setpoint),
       gas: gas}
   end
+
+  defp to_float(string) do
+    case Float.parse(string) do
+      {:ok, value} ->
+        value
+      {:error, msg} ->
+        Logger.info "unable to parse #{inspect msg}"
+        0
+    end
+  end
+
 end
