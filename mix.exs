6ea166a52c30fc5336c4157744ea67588cef0e67
Sven Bohm <bohms@hey.com>
2022-03-21 14:07:11 -0400

    updated elxir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Airflow.MixProject do
     [
       app: :airflow,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
