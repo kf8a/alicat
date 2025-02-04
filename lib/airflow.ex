1fcb8cdb598ba8f1a2a6d885767bde3c7b9695b5
Sven Bohm <bohms@hey.com>
2025-02-04 14:51:05 -0500

    only format changes

--- lib/airflow.ex
+++ lib/airflow.ex
@@ -1,4 +1,3 @@
 defmodule Airflow do
-  defstruct pressure: 0, temperature: 0, volumetric_flow: 0, mass_flow: 0,
-    setpoint: 0, gas: ""
+  defstruct pressure: 0, temperature: 0, volumetric_flow: 0, mass_flow: 0, setpoint: 0, gas: ""
 end
